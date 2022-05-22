efc3162bba07a8d29831bfa73be7c6c15bb77b5f
Developer184 <92087232+Developer184@users.noreply.github.com>
2022-05-22 05:13:19 -0700

    Initial commit

---
+++ src/form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function form() {
+    return (
+      <form>
+        <label>Enter your name:
+          <input type="text" />
+        </label>
+        <label>Enter your passward:
+          <input type="text" />
+        </label>
+        <label>
+            ]
+<input type="button"/>
+        </label>
+      </form>
+    )
+  }
+ 
